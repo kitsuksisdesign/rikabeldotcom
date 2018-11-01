c005ee1a67f2d074ac4b466aed9a5049a197c989
rikabelchristianaid <rabel@christian-aid.org>
2018-11-01 15:19:16 -0700

    updated title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Rikabel.me" />
     <Navbar />
     <div>{children}</div>
   </div>
